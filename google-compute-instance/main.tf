af774c62820014003a67793ce49ae78c0da0aed6
Sebastian Florek <sebastian@plural.sh>
2024-05-09 14:59:34 +0200

    update tf

--- google-compute-instance/main.tf
+++ google-compute-instance/main.tf
@@ -26,11 +26,6 @@ resource "google_compute_instance" "default" {
     }
   }
 
-  // Local SSD disk
-  scratch_disk {
-    interface = "NVME"
-  }
-
   network_interface {
     network = "default"
   }
