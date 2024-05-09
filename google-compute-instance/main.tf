39e1dd9b35a3f4fbbd8f1f9fbc177bc085ad7056
Sebastian Florek <sebastian@plural.sh>
2024-05-09 13:43:08 +0200

    update test scenarios

---
+++ google-compute-instance/main.tf
@@ -0,0 +1,45 @@
+resource "google_service_account" "default" {
+  account_id   = var.service_account_id
+  display_name = "Test Infrastructure Stack SA"
+  project      = var.project_id
+}
+
+resource "google_compute_instance" "default" {
+  name         = var.instance_name
+  project      = var.project_id
+  machine_type = var.machine_type
+  zone         = var.zone
+
+  tags = ["floreks", "infrastructure-stack", "test"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+      labels = {
+        my_label = "value"
+      }
+    }
+  }
+
+  // Local SSD disk
+  scratch_disk {
+    interface = "NVME"
+  }
+
+  network_interface {
+    network = "default"
+  }
+
+  metadata = {
+    owner = "floreks"
+    managed-by : "plural"
+  }
+
+  metadata_startup_script = "echo hi > /test.txt"
+
+  service_account {
+    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+    email  = google_service_account.default.email
+    scopes = ["cloud-platform"]
+  }
+}
