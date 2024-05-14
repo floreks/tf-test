4b7139953e20ecc16335341369dfba418380e220
Sebastian Florek <sebastian@plural.sh>
2024-05-14 11:28:09 +0200

    add long tf test stack

--- basic/main.tf
+++ basic/main.tf
@@ -7,7 +7,6 @@ resource "random_string" "random" {
 resource "null_resource" "default" {
   provisioner "local-exec" {
     command = "echo ${random_string.random.result}"
-
   }
 }
 
---
+++ long/main.tf
@@ -0,0 +1,32 @@
+variable "strings" {
+  default = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+  type = set(string)
+}
+
+resource "random_string" "random" {
+  for_each = { for s in var.strings : s => s }
+
+  length  = 16
+  upper   = false
+  special = false
+}
+
+resource "null_resource" "printer" {
+  for_each = random_string.random
+
+  provisioner "local-exec" {
+    command = "echo ${each.value.result}; sleep 10"
+  }
+
+  depends_on = [random_string.random]
+}
+
+output "sensitive" {
+  value = [ for i, s in random_string.random : s ]
+  sensitive =  true
+}
+
+output "standard" {
+  value = [ for i, s in random_string.random : s ]
+  sensitive =  false
+}
