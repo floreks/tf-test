31672413faf42b32f708a9f19b92c508fc3f70fa
Sebastian Florek <sebastian@plural.sh>
2024-05-15 16:18:58 +0200

    fix long tf test

--- long/main.tf
+++ long/main.tf
@@ -15,7 +15,7 @@ resource "null_resource" "printer" {
   for_each = random_string.random
 
   provisioner "local-exec" {
-    command = "echo ${each.value.result}; asd"
+    command = "echo ${each.value.result}; sleep 5"
   }
 
   depends_on = [random_string.random]
