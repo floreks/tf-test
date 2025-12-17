9aa31120e8b89ee09acec800220d84595ab1dace
Marcin Maciaszczyk <marcin@plural.sh>
2025-12-17 12:27:19 +0000

    feat: Add README.md and comments to code
    Adds a descriptive README.md file to the project and adds comments to the Ansible and Terraform code for better understanding.

--- terraform/google-compute-instance/main.tf
+++ terraform/google-compute-instance/main.tf
@@ -1,14 +1,18 @@
+# This configures the Google Cloud provider.
+# It uses variables for project ID and region.
 provider "google" {
   project     = var.project_id
   region      = var.region
 }
 
+# This resource creates a Google Service Account.
 resource "google_service_account" "default" {
   account_id   = var.service_account_id
   display_name = "Test Infrastructure Stack SA"
   project      = var.project_id
 }
 
+# This resource creates a Google Compute Engine instance.
 resource "google_compute_instance" "default" {
   name         = var.instance_name
   project      = var.project_id
@@ -17,6 +21,7 @@ resource "google_compute_instance" "default" {
 
   tags = ["floreks", "infrastructure-stack", "test"]
 
+  # This block configures the boot disk for the instance.
   boot_disk {
     initialize_params {
       image = "debian-cloud/debian-11"
@@ -26,17 +31,21 @@ resource "google_compute_instance" "default" {
     }
   }
 
+  # This block configures the network interface for the instance.
   network_interface {
     network = "default"
   }
 
+  # This block sets metadata for the instance.
   metadata = {
     owner = "floreks"
     managed-by : "plural"
   }
 
+  # This script runs on instance startup.
   metadata_startup_script = "echo hi > /test.txt"
 
+  # This block configures the service account for the instance.
   service_account {
     # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
     email  = google_service_account.default.email
--- terraform/variables/main.tf
+++ terraform/variables/main.tf
@@ -1,15 +1,18 @@
+# This resource generates a random string using input variables.
 resource "random_string" "random" {
   length  = var.random_string_length
   upper   = var.random_string_upper
   special = var.random_string_special
 }
 
+# This resource prints the generated random string.
 resource "null_resource" "default" {
   provisioner "local-exec" {
     command = "${var.null_resource_command} ${random_string.random.result}"
   }
 }
 
+# This output displays the generated random string.
 output "test" {
   value = random_string.random.result
 }
