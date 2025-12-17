fcd1242201d4f7b93a229777bc56272decd837f9
Marcin Maciaszczyk <marcin@plural.sh>
2025-12-17 14:02:30 +0000

    Update README.md and add comments to Ansible and Terraform files

--- terraform/google-compute-instance/main.tf
+++ terraform/google-compute-instance/main.tf
@@ -1,14 +1,17 @@
+# Configure the Google Cloud provider
 provider "google" {
   project     = var.project_id
   region      = var.region
 }
 
+# Create a service account
 resource "google_service_account" "default" {
   account_id   = var.service_account_id
   display_name = "Test Infrastructure Stack SA"
   project      = var.project_id
 }
 
+# Create a Google Compute Engine instance
 resource "google_compute_instance" "default" {
   name         = var.instance_name
   project      = var.project_id
@@ -17,6 +20,7 @@ resource "google_compute_instance" "default" {
 
   tags = ["floreks", "infrastructure-stack", "test"]
 
+  # Configure the boot disk
   boot_disk {
     initialize_params {
       image = "debian-cloud/debian-11"
@@ -26,6 +30,7 @@ resource "google_compute_instance" "default" {
     }
   }
 
+  # Use the default network interface
   network_interface {
     network = "default"
   }
@@ -35,8 +40,10 @@ resource "google_compute_instance" "default" {
     managed-by : "plural"
   }
 
+  # Script to run on startup
   metadata_startup_script = "echo hi > /test.txt"
 
+  # Attach the service account
   service_account {
     # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
     email  = google_service_account.default.email
