767824968b5bd18bdb7020c828f2466667aff0ee
Marcin Maciaszczyk <marcin@plural.sh>
2025-12-17 12:43:40 +0000

    docs: Update README and add comments to code

--- terraform/basic/main.tf
+++ terraform/basic/main.tf
@@ -1,15 +1,18 @@
+# Generate a random string of length 5
 resource "random_string" "random" {
   length  = 5
   upper   = false
   special = false
 }
 
+# Execute a local command to echo the generated string
 resource "null_resource" "default" {
   provisioner "local-exec" {
     command = "echo ${random_string.random.result}"
   }
 }
 
+# Output the generated random string
 output "test" {
   value = random_string.random.result
 }
--- terraform/google-compute-instance/main.tf
+++ terraform/google-compute-instance/main.tf
@@ -1,14 +1,17 @@
+# Configure the Google Cloud provider
 provider "google" {
   project     = var.project_id
   region      = var.region
 }
 
+# Create a Service Account
 resource "google_service_account" "default" {
   account_id   = var.service_account_id
   display_name = "Test Infrastructure Stack SA"
   project      = var.project_id
 }
 
+# Provision a Google Compute Engine instance
 resource "google_compute_instance" "default" {
   name         = var.instance_name
   project      = var.project_id
@@ -17,6 +20,7 @@ resource "google_compute_instance" "default" {
 
   tags = ["floreks", "infrastructure-stack", "test"]
 
+  # Boot disk configuration using Debian 11 image
   boot_disk {
     initialize_params {
       image = "debian-cloud/debian-11"
@@ -26,6 +30,7 @@ resource "google_compute_instance" "default" {
     }
   }
 
+  # Use default network interface
   network_interface {
     network = "default"
   }
@@ -35,8 +40,10 @@ resource "google_compute_instance" "default" {
     managed-by : "plural"
   }
 
+  # Startup script to run on boot
   metadata_startup_script = "echo hi > /test.txt"
 
+  # Attach the service account to the instance
   service_account {
     # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
     email  = google_service_account.default.email
