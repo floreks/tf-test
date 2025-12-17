8e9bd1a68a73d04e60cc7cc6928183c55e4e3f9b
Marcin Maciaszczyk <marcin@plural.sh>
2025-12-17 12:16:01 +0000

    feat: Update README.md and add comments to code
    Updated the README.md file to provide a clear and concise description of the project, including its purpose and how to use it.
    
    Added comments to the Terraform and Ansible files to explain the purpose of each script and the resources it creates.

--- terraform/variables/main.tf
+++ terraform/variables/main.tf
@@ -1,15 +1,18 @@
+# Creates a random string with a configurable length, and whether it should contain upper and special characters.
 resource "random_string" "random" {
   length  = var.random_string_length
   upper   = var.random_string_upper
   special = var.random_string_special
 }
 
+# Creates a null resource that executes a configurable command with the random string as an argument.
 resource "null_resource" "default" {
   provisioner "local-exec" {
     command = "${var.null_resource_command} ${random_string.random.result}"
   }
 }
 
+# Outputs the result of the random string.
 output "test" {
   value = random_string.random.result
 }
