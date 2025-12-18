cd94ac7891aa04c8f6d8b62569c55661ef9cf625
Marcin Maciaszczyk <marcin@plural.sh>
2025-12-18 14:16:52 +0000

    docs: update README and add comments to code
    Updated the README.md file to provide a better description of the project.
    Added comments to the Terraform and Ansible files to improve code readability.

--- terraform/basic/main.tf
+++ terraform/basic/main.tf
@@ -1,15 +1,22 @@
+# This Terraform configuration creates a random string and prints it to the console.
+
+# The "random_string" resource generates a random string of a specified length.
 resource "random_string" "random" {
   length  = 5
   upper   = false
   special = false
 }
 
+# The "null_resource" resource is a placeholder for running provisioners.
+# In this case, it uses the "local-exec" provisioner to execute a command on the local machine.
 resource "null_resource" "default" {
   provisioner "local-exec" {
+    # The command echoes the result of the random string to the console.
     command = "echo ${random_string.random.result}"
   }
 }
 
+# The "output" block defines an output variable that can be easily queried from the command line.
 output "test" {
   value = random_string.random.result
 }
--- terraform/long/main.tf
+++ terraform/long/main.tf
@@ -1,3 +1,6 @@
+# This Terraform configuration creates a set of random strings and prints them to the console.
+
+# The "strings" variable is a set of strings that will be used to create the random strings.
 variable "strings" {
   default = [
     "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
@@ -6,6 +9,7 @@ variable "strings" {
   type = set(string)
 }
 
+# The "random_string" resource creates a random string for each item in the "strings" variable.
 resource "random_string" "random" {
   for_each = { for i, s in var.strings : i => i }
 
@@ -14,9 +18,11 @@ resource "random_string" "random" {
   special = false
 }
 
+# The "null_resource" resource is used to print the random strings to the console.
 resource "null_resource" "printer" {
   for_each = random_string.random
 
+  # The "local-exec" provisioner runs a command on the local machine.
   provisioner "local-exec" {
     command = "echo ${each.value.result}; sleep 5"
   }
@@ -24,11 +30,13 @@ resource "null_resource" "printer" {
   depends_on = [random_string.random]
 }
 
+# The "sensitive" output variable will be redacted in the Terraform output.
 output "sensitive" {
   value = [ for i, s in random_string.random : s.result ]
   sensitive =  true
 }
 
+# The "standard" output variable will be displayed in the Terraform output.
 output "standard" {
   value = [ for i, s in random_string.random : s.result ]
   sensitive =  false
